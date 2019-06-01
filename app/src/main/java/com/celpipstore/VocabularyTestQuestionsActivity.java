cc1578a87b8ac67b295ecf33e1d5a53a944f39ee
cbitss_3 <cbitss_3@cbitss.com>
2019-06-01 17:39:42 +0530

    jjj

--- app/src/main/java/com/celpipstore/VocabularyTestQuestionsActivity.java
+++ app/src/main/java/com/celpipstore/VocabularyTestQuestionsActivity.java
@@ -4,7 +4,11 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
@@ -16,21 +20,42 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VocabularyTestQuestionsActivity extends AppCompatActivity {
     ListView listView;
+    Button button;
+    TextView textViewQuestion,textViewOption1,textViewOption2,textViewOption3,textViewOption4;
     String url;
+    int position =0;
+
+    List<String> ans;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vocabulary_test_questions);
+
+        ans = new ArrayList<>();
+        textViewQuestion = (TextView)findViewById(R.id.question);
+        textViewOption1 = findViewById(R.id.option1);
+        textViewOption2 = findViewById(R.id.option2);
+        textViewOption3 = findViewById(R.id.option3);
+        textViewOption4 = findViewById(R.id.option4);
+
         Intent intent = getIntent();
         String id = intent.getStringExtra("t1");
         url = "http://online.celpip.biz/api/vocabularyQuestion?lavelId="+id;
-        listView =(ListView)findViewById(R.id.listViewVocabularyTestQuestionsList);
+        Log.d("url", url);
+//        listView =(ListView)findViewById(R.id.listViewVocabularyTestQuestionsList);
         sendRequest();
-
-
-
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int p = ++position;
+                setData(p);
+            }
+        });
     }
 
     private void sendRequest() {
@@ -71,8 +96,18 @@ public class VocabularyTestQuestionsActivity extends AppCompatActivity {
     private void showJSON(String json) {
         JsonDataHandlerVocabularyTestQuestionList jsonHolderListing = new JsonDataHandlerVocabularyTestQuestionList(json);
         jsonHolderListing.parseJSON();
-        VocabularyTestQuestionAdapter ca = new VocabularyTestQuestionAdapter(this,jsonHolderListing.id,jsonHolderListing.level_id, jsonHolderListing.title,jsonHolderListing.question_image,jsonHolderListing.options1,jsonHolderListing.options2,jsonHolderListing.options3,jsonHolderListing.options4);
-        listView.setAdapter(ca);
-        ca.notifyDataSetChanged();
+
+        setData(position);
+//        VocabularyTestQuestionAdapter ca = new VocabularyTestQuestionAdapter(this,jsonHolderListing.id,jsonHolderListing.level_id, jsonHolderListing.title,jsonHolderListing.question_image,jsonHolderListing.options1,jsonHolderListing.options2,jsonHolderListing.options3,jsonHolderListing.options4);
+//        listView.setAdapter(ca);
+//        ca.notifyDataSetChanged();
+    }
+
+    private void setData(int position) {
+        textViewQuestion.setText(JsonDataHandlerVocabularyTestQuestionList.title[position]);
+        textViewOption1.setText(JsonDataHandlerVocabularyTestQuestionList.options1[position]);
+        textViewOption2.setText(JsonDataHandlerVocabularyTestQuestionList.options2[position+1]);
+        textViewOption3.setText(JsonDataHandlerVocabularyTestQuestionList.options3[position+2]);
+        textViewOption4.setText(JsonDataHandlerVocabularyTestQuestionList.options4[position+3]);
     }
 }
